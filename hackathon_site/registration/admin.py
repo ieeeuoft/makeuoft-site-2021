ea6431038d538d63a5850981690c200f8446ff98
Alex Bogdan <stefanalexb@gmail.com>
2021-01-04 14:36:02 -0500

    Added csv download functionality for Applications page in the Admin site

--- hackathon_site/registration/admin.py
+++ hackathon_site/registration/admin.py
@@ -1,11 +1,52 @@
+import csv
+import functools
+
 from django.contrib import admin
+from django.http import HttpResponse
+
 from registration.models import Application, Team as TeamApplied
+
+
+def rgetattr(obj, attr, *args):
+    """
+    Recursive getattr, allows for nested attributes.
+    For example if you're in the Application class, you can't do getattr for user.first_name
+    But with rgetattr you can.
+    """
+
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+
+    return functools.reduce(_getattr, [obj] + attr.split("."))
 
 
 class ApplicationInline(admin.TabularInline):
     model = Application
     autocomplete_fields = ("user",)
     extra = 0
+
+
+class ExportCsvMixin:
+    export_field_names = []
+    export_field_attributes = []
+
+    def export_as_csv(self, request, queryset):
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(
+            self.model._meta
+        )
+        writer = csv.writer(response)
+
+        writer.writerow(self.export_field_names)
+        for obj in queryset:
+            attributes = [
+                rgetattr(obj, field) for field in self.export_field_attributes
+            ]
+            row = writer.writerow(attributes)
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 @admin.register(TeamApplied)
@@ -24,7 +65,7 @@ class TeamAppliedAdmin(admin.ModelAdmin):
 
 
 @admin.register(Application)
-class ApplicationAdmin(admin.ModelAdmin):
+class ApplicationAdmin(admin.ModelAdmin, ExportCsvMixin):
     autocomplete_fields = ("user", "team")
     list_display = ("get_full_name", "team", "school")
     search_fields = (
@@ -34,6 +75,42 @@ class ApplicationAdmin(admin.ModelAdmin):
         "team__team_code",
     )
     list_filter = ("school",)
+    actions = ["export_as_csv"]
+
+    export_field_names = [
+        "First Name",
+        "Last Name",
+        "Email",
+        "Team Code",
+        "Birthday",
+        "Gender",
+        "Ethnicity",
+        "School",
+        "Study Level",
+        "Graduation Year",
+        "Resume Sharing",
+        "Review Status",
+        "RSVP",
+        "Created At",
+        "Updated At",
+    ]
+    export_field_attributes = [
+        "user.first_name",
+        "user.last_name",
+        "user",
+        "team",
+        "birthday",
+        "gender",
+        "ethnicity",
+        "school",
+        "study_level",
+        "graduation_year",
+        "resume_sharing",
+        "review.status",
+        "rsvp",
+        "created_at",
+        "updated_at",
+    ]
 
     def get_full_name(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
